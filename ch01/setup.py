1cd15da2492cddfbb673867254d94186ce55105a
Jens Albrecht <jsalbr@gmail.com>
2020-09-06 20:25:56 +0200

    minor changes

--- ch01/setup.py
+++ ch01/setup.py
@@ -6,18 +6,17 @@ if ON_COLAB:
     print("You are working on Google Colab.")
     print(f'Files will be downloaded to "{BASE_DIR}".')
 else:
-    BASE_DIR = ".."
+    BASE_DIR = "."
     print("You are working on a local system.")
     print(f'Files will be searched relative to "{BASE_DIR}".')
 
 if ON_COLAB:
-    # there are some generic files, maybe externalize?
     required_files = [
         'settings.py',
         'data/un-general-debates/un-general-debates-blueprint.csv.gz',
         'ch01/requirements.txt'
     ]
-                  
+
     print("Downloading required files ...")
     for file in required_files:
         cmd = ' '.join(['wget', '-P', os.path.dirname(BASE_DIR+'/'+file), GIT_ROOT+'/'+file])
@@ -26,12 +25,9 @@ if ON_COLAB:
             print('  --> ERROR')
 
     print("\nAdditional setup ...")
-    setup_cmds = [f'mkdir -p {BASE_DIR}/data/un-general-debates',
-                  'pip install -r ch13/colab_requirements.txt',
-                  'python -m spacy download en']
+    setup_cmds = ['pip install -r ch01/requirements.txt']
 
     for cmd in setup_cmds:
         print('!'+cmd)
         if os.system(cmd) != 0:
             print('  --> ERROR')
-           
