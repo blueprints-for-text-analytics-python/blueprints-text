422566c769bfc0dac8421fd4f949bbcd31167ab2
sidhusmart <sidharth.ramachandran@gmail.com>
2020-10-24 14:42:39 +0200

    Adding notebooks for ch06

---
+++ ch06/setup.py
@@ -0,0 +1,24 @@
+# setup for chapter 06 
+import os
+
+if ON_COLAB:
+    BASE_DIR = "/content"
+    print("You are working on Google Colab.")
+    print(f'Files will be downloaded to "{BASE_DIR}".')
+else:
+    BASE_DIR = ".."
+    print("You are working on a local system.")
+    print(f'Files will be searched relative to "{BASE_DIR}".')
+
+if ON_COLAB:
+    required_files = [
+        'settings.py',
+        'data/jdt-bugs-dataset/eclipse_jdt.csv.gz',
+    ]
+
+    print("Downloading required files ...")
+    for file in required_files:
+        cmd = ' '.join(['wget', '-P', os.path.dirname(BASE_DIR+'/'+file), GIT_ROOT+'/'+file])
+        print('!'+cmd)
+        if os.system(cmd) != 0:
+            print('  --> ERROR')
