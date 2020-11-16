9a13797fb4d9f807f459fd80dd96147d7b6d25ad
Christian Winkler <christian.winkler@datanizing.com>
2020-11-16 14:55:13 +0100

    notebooks working locally

---
+++ ch05/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+import sys, os
+
+GIT_ROOT = 'https://github.com/blueprints-for-text-analytics-python/blueprints-text/raw/master'
+
+# this is generic, maybe externalize?
+ON_COLAB = 'google.colab' in sys.modules
+if ON_COLAB:
+    BASE_DIR = "/content"
+    print("You are working on Google Colab.")
+    print(f'Files will be downloaded to "{BASE_DIR}".')
+else:
+    BASE_DIR = ".."
+    print("You are working on a local system.")
+    print(f'Files will be searched relative to "{BASE_DIR}".')
+
+def universal_filename(f):
+    return os.path.join(BASE_DIR, f)
+
+
+
+_ABCNEWS_FILE = os.path.join('data', 'abcnews', 'abcnews-date-text.csv.gz')
+ABCNEWS_FILE = universal_filename(_ABCNEWS_FILE)
+
+if ON_COLAB:
+    # there are some generic files, maybe externalize?
+    required_files = [
+                  'settings.py',
+                  _DEBATES_FILE,
+                  'ch05/colab_requirements.txt'
+    ]
+    print("Downloading required files ...")
+    for file in required_files:
+        cmd = ' '.join(['wget', '-P', os.path.dirname(BASE_DIR+'/'+file), GIT_ROOT+'/'+file])
+        print('!'+cmd)
+        os.system(cmd)
+
+
