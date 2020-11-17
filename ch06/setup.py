ad78cf7c7de6e5d49c8a686f949d5226bd9711ff
sidhusmart <sidharth.ramachandran@gmail.com>
2020-11-17 20:35:26 +0100

    Path and Package changes

--- ch06/setup.py
+++ ch06/setup.py
@@ -14,12 +14,23 @@ if ON_COLAB:
     required_files = [
         'settings.py',
         'data/jdt-bugs-dataset/eclipse_jdt.csv.gz',
-	'packages/blueprints/preparation.py'
+	'packages/blueprints/preparation.py',
+	'ch06/requirements.txt'
     ]
 
     print("Downloading required files ...")
     for file in required_files:
         cmd = ' '.join(['wget', '-P', os.path.dirname(BASE_DIR+'/'+file), GIT_ROOT+'/'+file])
+        print('!'+cmd)
+        if os.system(cmd) != 0:
+            print('  --> ERROR')
+
+    print("\nAdditional setup ...")
+    setup_cmds = [
+        'pip install -r ch06/requirements.txt'
+    ]
+
+    for cmd in setup_cmds:
         print('!'+cmd)
         if os.system(cmd) != 0:
             print('  --> ERROR')
